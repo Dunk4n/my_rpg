712918786faca2cc265cdfe0d33147adae9153db
Victor Rouxel <victor.rouxel@epitech.eu>
2019-05-05 22:13:59 +0200

    items mecanism

--- src/inventory.c
+++ src/inventory.c
@@ -7,7 +7,7 @@
 
 #include "my.h"
 
-static void show_items(my_game_t *game, item_t *items)
+static void show_items(play_t *play, my_game_t *game, item_t *items)
 {
     sfSprite_setPosition((items[0]).sprite, (sfVector2f) 
     {1715.0 / 4 , 185.0 / 4});
@@ -31,15 +31,9 @@ static void inv_loop(play_t *play, my_game_t *game)
     while (sfRenderWindow_isOpen(game->win->window)) {
         mouse = sfMouse_getPositionRenderWindow(game->win->window);
         sfRenderWindow_drawSprite(game->win->window, play->s_inv, NULL);
-<<<<<<< HEAD
         show_items(play, game, play->items);
         if (mouse.x >= 1370 && mouse.x <= 1580 && mouse.y >= 80 &&
             mouse.y <= 210 && sfMouse_isButtonPressed(sfMouseLeft)) {
-=======
-        show_items(game, play->items);
-        if (mouse.x >= 1370 && mouse.x <= 1480 && mouse.y >= 80 &&
-            mouse.y <= 160 && sfMouse_isButtonPressed(sfMouseLeft)) {
->>>>>>> 94cd0799c9d84eb197b1874eaf66c322240ac96c
             play->inventory = false;
             break;
         }
@@ -51,10 +45,6 @@ static void inv_loop(play_t *play, my_game_t *game)
 void inventory(play_t *play, my_game_t *game)
 {
     play->inventory = !play->inventory;
-<<<<<<< HEAD
-
-=======
->>>>>>> 94cd0799c9d84eb197b1874eaf66c322240ac96c
     if (play->inventory == false)
         return;
     inv_loop(play, game);
